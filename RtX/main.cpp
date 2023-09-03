334af0e34b2cd412f0fd0c389d0d1bc8383b9f96
Prakhar Prabir <prakhar.krs@outlook.com>
2023-09-03 20:57:45 +0530

    project setup

---
+++ RtX/main.cpp
@@ -0,0 +1,34 @@
+﻿#include "pch.h"
+
+
+int main()
+{
+	
+	if (glfwInit() != GLFW_TRUE) {
+		std::cerr << "Failed to initialize glfw." << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	if (volkInitialize() != VK_SUCCESS) {
+		std::cerr << "Failed to find vulkan loader on host system" << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	GLFWwindow* window = glfwCreateWindow(1280, 720, "RtX", nullptr, nullptr);
+	if (!window) {
+		std::cerr << "Failed to create glfw window." << std::endl;
+		glfwTerminate();
+		return EXIT_FAILURE;
+	}
+
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+
+	return 0;
+}
---
+++ RtX/pch.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+#ifndef VK_NO_PROTOTYPES
+#define VK_NO_PROTOTYPES
+#endif //VK_NO_PROTOTYPES
+
+#ifndef GLFW_INCLUDE_VULKAN
+#define GLFW_INCLUDE_VULKAN
+#endif //GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include "../ext/volk/volk.h"
+
+#include <iostream>
+
